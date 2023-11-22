030fe3029636c1464fdafd4c24567be6628376f7
Louis <louis.reynaud@imt-atlantique.net>
2023-11-22 11:42:18 +0100

    commit pour Titouen

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -86,10 +86,11 @@ export const listUsers = async (
 };
 
 export const addUser = async (request: FastifyRequest, reply: FastifyReply) => {
+  let namea = "Maxooo" as db.SQL;
   return db.sql<
     s.users.SQL,
     s.users.Insertable[]
-  >`INSERT INTO ${"users"} (email, password) VALUES ('example@example.com', 'motdepasse123')`
+  >`INSERT INTO ${"users"} (${"email"}, password) VALUES ('example@example.com', 'motdepasse123')`
     .run(pool)
     .then((users) => ({ data: users }));
   // Or .then((users) => reply.send({ data: users }))
--- src/db/initDB.ts
+++ src/db/initDB.ts
@@ -2,10 +2,10 @@ import * as db from "zapatos/db";
 
 export const initDB = async () => {
   db.sql`
-        CREATE TABLE IF NOT EXISTS users (
-            id SERIAL PRIMARY KEY,
-            email TEXT NOT NULL UNIQUE,
-            password TEXT NOT NULL
-        );
+  CREATE TABLE IF NOT EXISTS users (
+    user_id SERIAL PRIMARY KEY,
+    name	TEXT NOT NULL,
+    score   INTEGER DEFAULT 0
+  )  
     `;
 };
